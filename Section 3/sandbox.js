7ee9422fd4ac9bd5ab6bdbf611afd77f99df9504
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-21 13:00:16 -0400

    Continue and Break Loop

--- Section 3/sandbox.js
+++ Section 3/sandbox.js
@@ -1,5 +1,13 @@
-let nameArray = ["Zane", "Sean", "John", "Kingston"];
-for (let i = 0; i < nameArray.length; i++) {
-  let htmlBlock = `<div>${nameArray[i]}</div>`;
-  console.log(htmlBlock);
+const scores = [0, 10, 20, 30, 40, 50, 60, 70];
+
+for (let i = 0; i < scores.length; i++) {
+    if(scores[i] == 40){
+    continue;
+    }
+  console.log(`Your score is ${scores[i]}`);
+
+  if(scores[i] == 50){
+    console.log('Score is 30');
+    break;
+  }
 }
