7b766a6ef8e238a392b4fe47468a12d5035668a2
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-24 11:44:46 -0400

    Creating an Object Literal

---
+++ Section 5/sandbox.js
@@ -0,0 +1,11 @@
+// Object Literals
+
+let user = {
+    name: 'Zane',
+    age: 23,
+    email: 'zane.birkett@caricom.org',
+    location: 'Guyana',
+    blogs: ['Dog are better than cats','Guyana is great']
+};
+
+console.log(user);
