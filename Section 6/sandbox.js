afcb43bed7e71c5c798273968dcfc27a3e10c949
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-24 14:38:41 -0400

    Changing CSS Styles

--- Section 6/sandbox.js
+++ Section 6/sandbox.js
@@ -1,10 +1,17 @@
-const link = document.querySelector('a');
-console.log(link.getAttribute('href'));
+// const link = document.querySelector('a');
+// console.log(link.getAttribute('href'));
 
-link.setAttribute('href', 'thenetninja.co.uk');
-console.log(link.getAttribute("href"));
+// link.setAttribute('href', 'thenetninja.co.uk');
+// console.log(link.getAttribute("href"));
 
-const mssg = document.querySelector('p');
-console.log(mssg.getAttribute('class'))
-mssg.setAttribute('class','success')
-mssg.setAttribute('style', 'color:green;');
+// const mssg = document.querySelector('p');
+// console.log(mssg.getAttribute('class'))
+// mssg.setAttribute('class','success')
+// mssg.setAttribute('style', 'color:green;');
+
+const header = document.querySelector('h1');
+// header.setAttribute('style','margin:50px;')
+console.log(header.style.color)
+header.style.margin = '50px';
+header.style.color = 'crimson'
+header.style.fontSize = '100px'
