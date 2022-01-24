c68b92b219a921e03c2e5a1c8f7c395b797fed7c
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-24 14:53:09 -0400

    Adding & Removing Classes

--- Section 6/sandbox.js
+++ Section 6/sandbox.js
@@ -1,17 +1,31 @@
-// const link = document.querySelector('a');
-// console.log(link.getAttribute('href'));
+// const content = document.querySelector('p');
+// content.className = 'success';
 
-// link.setAttribute('href', 'thenetninja.co.uk');
-// console.log(link.getAttribute("href"));
+// console.log(content.classList);
+// content.classList.add('success');
 
-// const mssg = document.querySelector('p');
-// console.log(mssg.getAttribute('class'))
-// mssg.setAttribute('class','success')
-// mssg.setAttribute('style', 'color:green;');
+// My way
+// const paras = document.querySelectorAll('p')
+// paras.forEach(
+//     para => {
+//         if(para.innerText.includes('error')){
+//             para.classList.add('error');
+//         }else if(para.innerText.includes('success')){
+//             para.classList.add('success');
+//         }
+//     }
+// )
 
-const header = document.querySelector('h1');
-// header.setAttribute('style','margin:50px;')
-console.log(header.style.color)
-header.style.margin = '50px';
-header.style.color = 'crimson'
-header.style.fontSize = '100px'
+const paras = document.querySelectorAll("p");
+paras.forEach(para => {
+  if (para.textContent.includes('error')) {
+    para.classList.add("error");
+  }
+  if (para.textContent.includes('success')) {
+    para.classList.add("success");
+  }
+});
+
+const title = document.querySelector('.title');
+title.classList.toggle('test');
+title.classList.toggle('test');
