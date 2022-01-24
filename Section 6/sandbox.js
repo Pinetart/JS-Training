8a235f4df783ae2642d8bb8086bb2fe6ecda63c1
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-24 15:47:11 -0400

    Creating & Removing Elements

--- Section 6/sandbox.js
+++ Section 6/sandbox.js
@@ -1,15 +1,17 @@
-const button = document.querySelector('button')
-
-button.addEventListener('click',() =>{
-    console.log('You clicked me')
-})
-
+const ul = document.querySelector('ul');
+const button = document.querySelector('button');
 const listNodes = document.querySelectorAll('li');
-listNodes.forEach(node => {
-    node.addEventListener('click', () => {
-    // console.log('item clicked')
-    // console.log(e.target)
-    // console.log(node)
-    node.style.textDecoration = 'line-through';
+
+button.addEventListener('click',() => {
+    // ul.innerHTML += '<li>Something new</li>'
+    const li = document.createElement('li');
+    li.textContent = 'Something new';
+    ul.append(li)
 })
+
+listNodes.forEach(node => {
+        node.addEventListener('click', () => {
+        // node.style.textDecoration = 'line-through';
+        node.remove();
+    })
 })
