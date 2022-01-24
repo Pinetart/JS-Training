27ea46cfb4370db1dcf8b8299075c2acb7b95dcc
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-24 11:48:57 -0400

    Adding Methods

--- Section 5/sandbox.js
+++ Section 5/sandbox.js
@@ -1,11 +1,22 @@
 // Object Literals
 
 let user = {
-    name: 'Zane',
-    age: 23,
-    email: 'zane.birkett@caricom.org',
-    location: 'Guyana',
-    blogs: ['Dog are better than cats','Guyana is great']
+  name: "Zane",
+  age: 23,
+  email: "zane.birkett@caricom.org",
+  location: "Guyana",
+  blogs: ["Dog are better than cats", "Guyana is great"],
+  login: () => {
+    console.log("the user is logged in");
+  },
+  logout: () => {
+    console.log("the user is logged out");
+  },
+  logBlogs: () => {
+      
+  }
 };
 
-console.log(user);
+user.login();
+user.logout();
+
