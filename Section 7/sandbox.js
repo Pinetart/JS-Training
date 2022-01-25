07d2b8e5d3a56959f072c1b35fb3231898065f6e
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-25 10:39:00 -0400

    Keyboard Events

--- Section 7/sandbox.js
+++ Section 7/sandbox.js
@@ -1,5 +1,4 @@
 const form = document.querySelector('.signup-form');
-// const username = document.querySelector('#username');
 const feedback = document.querySelector('.feedback');
 
 form.addEventListener('submit', e => {
@@ -14,3 +13,14 @@ form.addEventListener('submit', e => {
     feedback.textContent = 'Username must only contain letters and must be 6-12 characters long'
   }
 })
+
+form.username.addEventListener('keyup', (e) => {
+  // console.log(e.target.value, form.username.value)
+  const userRgx = /^[a-zA-Z]{6,12}$/;
+
+  if (userRgx.test(e.target.value)) {
+    form.username.setAttribute('class','success');
+  } else {
+    form.username.setAttribute('class', 'error');
+  }
+});
