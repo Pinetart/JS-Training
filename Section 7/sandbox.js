c85b93c64f89cb344bb9dfd097623d89c468dbce
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-25 10:20:40 -0400

    Basic Form Validation

--- Section 7/sandbox.js
+++ Section 7/sandbox.js
@@ -1,8 +1,16 @@
 const form = document.querySelector('.signup-form');
 // const username = document.querySelector('#username');
+const feedback = document.querySelector('.feedback');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
-  // console.log(username.value);
-  console.log(form.username.value);
+  const username = form.username.value;
+  const userRgx = /^[a-zA-Z]{6,12}$/
+
+  if(userRgx.test(username)){
+    // console.log('Username valid!')
+    feedback.textContent = 'Username valid!'
+  } else{
+    feedback.textContent = 'Username must only contain letters and must be 6-12 characters long'
+  }
 })
