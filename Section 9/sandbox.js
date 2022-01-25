d268f6c4449aa0f3c7cf39daa385cf87b686df18
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-25 13:19:37 -0400

    Reduct Method

--- Section 9/sandbox.js
+++ Section 9/sandbox.js
@@ -1,24 +1,24 @@
-const prices = [20, 10, 30, 25, 15, 40, 80, 6];
+// const reducScores = scores.reduce((acc, curr) => {
+//   if (curr > 50) {
+//     acc++;
+//   }
+//   return acc;
+// }, 0);
 
-// const salePrices = prices.map((price) => price / 2);
-// console.log(salePrices)
+// console.log(reducScores)
 
-const products = [
-  { name: "gold star", price: 20 },
-  { name: "mushroom", price: 40 },
-  { name: "green shells", price: 30 },
-  { name: "banana skin", price: 10 },
-  { name: "red shells", price: 50 },
+const scores = [
+  { player: "mario", score: 50 },
+  { player: "yoshi", score: 30 },
+  { player: "mario", score: 70 },
+  { player: "crystal", score: 60 },
 ];
 
-const saleProducts = products.map((product) => {
-  if (product.price > 30) {
-    // product.price = product.price / 2;
-    // return product
-    return { name: product.name, price: product.price / 2 };
-  } else {
-    return product;
+const marioTotal = scores.reduce((acc, curr) => {
+  if (curr.player === "mario") {
+    acc += curr.score;
   }
-});
+  return acc;
+}, 0);
 
-console.log(saleProducts, products);
+console.log(marioTotal);
