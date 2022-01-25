f551eaa4714bd45d9a9b9ce0f2ed14e036e84a98
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-25 18:45:15 -0400

    Date-fns Library

--- Section 11/sandbox.js
+++ Section 11/sandbox.js
@@ -1,18 +1,15 @@
-const clock = document.querySelector(".clock");
+const now = new Date();
 
-const tick = () => {
-  const now = new Date();
-  const h = now.getHours();
-  const m = now.getMinutes();
-  const s = now.getSeconds();
+console.log(dateFns.isToday(now));
 
-  const html = `
-    <span>${h}</span> :
-    <span>${m}</span> :
-    <span>${s}</span> 
-  `;
+// formatting options
+console.log(dateFns.format(now, "YYYY"));
+console.log(dateFns.format(now, "MMMM"));
+console.log(dateFns.format(now, "dddd"));
+console.log(dateFns.format(now, "Do"));
+console.log(dateFns.format(now, "dddd, Do MMMM, YYYY"));
 
-  clock.innerHTML = html
-};
+// comparing dates
+const before = new Date("February 1 2019 12:00:00");
 
-setInterval(tick, 1000);
+console.log(dateFns.distanceInWords(now, before, { addSuffix: true }));
