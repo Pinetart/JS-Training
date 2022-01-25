d120cc6938297e322db534a9ec227a3e307fc631
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-25 13:33:16 -0400

    Sort Method

--- Section 9/sandbox.js
+++ Section 9/sandbox.js
@@ -1,5 +1,30 @@
-const scores = [10, 5, 0, 40, 60, 10, 20, 70];
+const names = ["mario", "shaun", "chun-li", "yoshi", "luigi"];
+const scores = [10, 20, 30, 40, 50];
 
-const firstHighScore = scores.find((score) => score > 50);
+// names.sort();
+// console.log(names)
 
-console.log(firstHighScore);
+// names.reverse();
+// console.log(names)
+
+const players = [
+  { name: "mario", score: 20 },
+  { name: "luigi", score: 10 },
+  { name: "chun-li", score: 50 },
+  { name: "yoshi", score: 30 },
+  { name: "shaun", score: 70 },
+];
+
+// players.sort((a, b) => {
+//   if (a.score > b.score) {
+//     return -1;
+//   } else if (b.score > a.score) {
+//     return 1;
+//   } else {
+//     0;
+//   }
+// });
+
+players.sort((a, b) => b.score - a.score);
+
+console.log(players);
