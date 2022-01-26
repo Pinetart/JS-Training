fcd70a9de5cf7fc8c7ab5876406e31e8cc063db9
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-26 14:32:29 -0400

    Get Weather API Call

--- Section 13/scripts/forecast.js
+++ Section 13/scripts/forecast.js
@@ -1,5 +1,17 @@
 const key = "kBuIpRH9VukfyFBbhH9R32Lk20R0sWNG";
 
+// get weather information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city information
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
@@ -10,10 +22,13 @@ const getCity = async (city) => {
   return data[0];
 };
 
-getCity("Georgetown")
+getCity("georgetown")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
   .then((data) => {
     console.log(data);
   })
   .catch((err) => {
-    console.log(err);
+    console.log(err.message);
   });
