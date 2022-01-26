868c300eb7fea54174ed09b647e5a9c1f0a3c567
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-26 12:35:10 -0400

    Async & Await

--- Section 12/sandbox.js
+++ Section 12/sandbox.js
@@ -1,10 +1,24 @@
-fetch("todos/luigi.json")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("error: ", err);
-  });
+// fetch("todos/luigi.json")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("error: ", err);
+//   });
+
+// const getTodos = async () => {
+//   const response = await fetch("todos/luigi.json");
+//   const data = await response.json();
+//   return data;
+// };
+// console.log(getTodos());
+
+const getShort = async (resource) => {
+  const information = await (await fetch(resource)).json();
+  return information;
+};
+
+getShort("todos/luigi.json").then((data) => console.log("resolved:", data));
