0503981b63ab7982f4c11f408f9b6bdc7b6b584a
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-26 11:56:29 -0400

    Promise - Basics

--- Section 12/sandbox.js
+++ Section 12/sandbox.js
@@ -1,25 +1,45 @@
-const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest();
+const getTodos = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-  request.addEventListener("readystatechange", () => {
-    //   console.log(request, request.readyState);
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback("Could not fetched data", undefined);
-    }
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject("error getting resource");
+      }
+    });
+    request.open("GET", resource);
+    request.send();
   });
-  request.open("GET", resource);
-  request.send();
 };
 
-getTodos("todos/luigi.json", (err, data) => {
-  console.log(data);
-  getTodos("todos/mario.json", (err, data) => {
-    console.log(data);
-    getTodos("todos/shaun.json", (err, data) => {
-      console.log(data);
-    });
+getTodos("todos/luigi.json")
+  .then((data) => {
+    console.log("promise resolved", data);
+  })
+  .catch((err) => {
+    console.log(err);
   });
-});
+
+// const getSomething = () => {
+//   return new Promise((resolve, reject) => {
+//     // resolve("some data");
+//     reject("some error");
+//   });
+// };
+
+// getSomething().then((data) => {
+//   console.log(data);
+// }, (err) => {
+//     console.log(err)
+// });
+
+// getSomething()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
