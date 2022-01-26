2ec682797334e6a305aff830df3c12c87c0ed0f1
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-26 14:13:47 -0400

    Get City API Call

--- Section 13/scripts/forecast.js
+++ Section 13/scripts/forecast.js
@@ -1,1 +1,19 @@
 const key = "kBuIpRH9VukfyFBbhH9R32Lk20R0sWNG";
+
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity("Georgetown")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
