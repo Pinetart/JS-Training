6f52d4a73389b1f9782ebf826ed56d2bf2f4a399
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-26 12:19:17 -0400

    The Fetch API

--- Section 12/sandbox.js
+++ Section 12/sandbox.js
@@ -1,53 +1,10 @@
-const getTodos = (resource) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener("readystatechange", () => {
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4) {
-        reject("error getting resource");
-      }
-    });
-    request.open("GET", resource);
-    request.send();
-  });
-};
-
-getTodos("todos/luigi.json")
-  .then((data) => {
-    console.log("promise resolved:", data);
-    return getTodos("todos/mario.json");
+fetch("todos/luigi.json")
+  .then((response) => {
+    return response.json();
   })
   .then((data) => {
-    console.log("Promise 2 resolved:", data);
-    return getTodos("todos/shaun.json");
-  })
-  .then(() => {
-    console.log("Promise 3 resolved:", data);
+    console.log(data);
   })
   .catch((err) => {
-    console.log("Promise rejected:", err);
+    console.log("error: ", err);
   });
-
-// const getSomething = () => {
-//   return new Promise((resolve, reject) => {
-//     // resolve("some data");
-//     reject("some error");
-//   });
-// };
-
-// getSomething().then((data) => {
-//   console.log(data);
-// }, (err) => {
-//     console.log(err)
-// });
-
-// getSomething()
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
