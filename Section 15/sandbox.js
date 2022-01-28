537ac61b97d187a0a0d6ab8348b9ee374b87ccbd
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-28 13:51:42 -0400

    Class Methods and Chaining Methods

--- Section 15/sandbox.js
+++ Section 15/sandbox.js
@@ -1,9 +1,27 @@
 class User {
-  constructor(username, email){
-    this.username = username
-    this.email = email
+  constructor(username, email) {
+    this.username = username;
+    this.email = email;
+    this.score = 0;
+  }
+  login() {
+    console.log(`${this.username} just logged in`);
+    return this;
+  }
+  logout() {
+    console.log(`${this.username} just logged out`);
+    return this;
+  }
+  incScore() {
+    this.score++;
+    console.log(`${this.username} has a score of ${this.score}`)
+    return this;
   }
 }
 
-const test = new User('zane','zanebirkett@icloud.com')
-console.log(test)
+const test = new User("zane", "zanebirkett@icloud.com");
+const test2 = new User("Mario", "Mario@gmail.com");
+// console.log(test, test2);
+// test.login().logout();
+test.incScore().incScore().incScore().logout();
+
