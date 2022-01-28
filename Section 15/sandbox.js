fbde907fd7952ae3c74f0807985bef8253e63a57
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-28 14:14:09 -0400

    Class Inheritance

--- Section 15/sandbox.js
+++ Section 15/sandbox.js
@@ -14,14 +14,29 @@ class User {
   }
   incScore() {
     this.score++;
-    console.log(`${this.username} has a score of ${this.score}`)
+    console.log(`${this.username} has a score of ${this.score}`);
     return this;
+  }
+}
+
+class Admin extends User {
+  deleteUser(user) {
+    users = users.filter((u) => {
+      return u.username !== user.username;
+    });
   }
 }
 
 const test = new User("zane", "zanebirkett@icloud.com");
 const test2 = new User("Mario", "Mario@gmail.com");
+const test3 = new Admin("axel", "axelbirkett@icloud.com");
 // console.log(test, test2);
 // test.login().logout();
-test.incScore().incScore().incScore().logout();
+// test.incScore().incScore().incScore().logout();
+// console.log(test3)
 
+let users = [test, test2, test3];
+console.log(users);
+
+test3.deleteUser(test2);
+console.log(users);
