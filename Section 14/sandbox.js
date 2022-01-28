33fff7f6b84c3af449b7bad84e1e4493868401a1
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-01-28 10:46:18 -0400

    Storing & Getting Data

---
+++ Section 14/sandbox.js
@@ -0,0 +1,10 @@
+// localStorage.setItem("name", "Mario");
+// localStorage.setItem("age", 50);
+
+// let name = localStorage.getItem("name");
+// let age = localStorage.getItem("age");
+// console.log(name, age);
+
+localStorage.setItem('key','XBOA!@)!OSAX')
+let keyString = localStorage.getItem('key')
+console.log(keyString)
