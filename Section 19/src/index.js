f808a85c8e619024a19cb489d0a10a7ee57c55a6
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-02-01 13:15:07 -0400

    NPM Scripts & Watching Files

---
+++ Section 19/src/index.js
@@ -0,0 +1,13 @@
+const greet = (name) => {
+  console.log(`Hello ${name}`);
+};
+
+greet("mario");
+greet("luigi");
+greet("dogma");
+
+class User{
+    constructor(){
+        this.score = 0;
+    }
+}
